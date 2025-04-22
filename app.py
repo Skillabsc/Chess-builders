702f0f5ef4d15f8ecfcc4611d35ed970a87092a4
Skillabsc <161401031+Skillabsc@users.noreply.github.com>
2025-04-22 15:31:05 +0700

    Add backend

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def reset_board():
     return jsonify({'status': 'success', 'fen': board.fen()})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000) 
